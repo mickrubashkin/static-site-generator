9de71a03ee35ecf8f7eb2fa35303f64a2b09ce6f
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-12-01 08:44:49 +0100

    rebuild website to docs for publishing

--- src/build_content.py
+++ src/build_content.py
@@ -44,7 +44,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("no title found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if not os.path.exists(from_path):
         raise ValueError(f"non existent from_path: {from_path}")
     if not os.path.exists(template_path):
@@ -59,13 +59,15 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md)
     full_html = template.replace("{{ Title }}", title)
     full_html = full_html.replace("{{ Content }}", html_content)
+    full_html = full_html.replace("href=\"/", f"href={basepath}")
+    full_html = full_html.replace("src=\"/", f"src={basepath}")
     dirpath = os.path.dirname(dest_path)
     if dirpath:
         os.makedirs(dirpath, exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(full_html)
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dir_path_content):
         raise ValueError(f"non existent dir_path_content: {dir_path_content}")
     if not os.path.exists(template_path):
@@ -80,6 +82,6 @@ def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_filename = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_filename)
+            generate_page(from_path, template_path, dest_filename, basepath)
         else:
-            generate_pages_recursively(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from build_content import copy_content, generate_page, generate_pages_recursively
+import sys
+from build_content import copy_content, generate_page, generate_pages_recursive
 
-
-print("hello world")
 
 def main():
-    copy_content("static", "public")
-    generate_pages_recursively("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_content("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
