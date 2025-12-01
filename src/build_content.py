27689abdaf832541283e34e20dc4c2928dfacf84
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-12-01 08:50:26 +0100

    fix typo in href/src replacing, rebuild docs dir

--- src/build_content.py
+++ src/build_content.py
@@ -59,8 +59,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     title = extract_title(md)
     full_html = template.replace("{{ Title }}", title)
     full_html = full_html.replace("{{ Content }}", html_content)
-    full_html = full_html.replace("href=\"/", f"href={basepath}")
-    full_html = full_html.replace("src=\"/", f"src={basepath}")
+    full_html = full_html.replace("href=\"/", f"href=\"{basepath}")
+    full_html = full_html.replace("src=\"/", f"src=\"{basepath}")
     dirpath = os.path.dirname(dest_path)
     if dirpath:
         os.makedirs(dirpath, exist_ok=True)
